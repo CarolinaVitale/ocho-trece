18a23999b095fd0cbb2a11c2b0e0dc1a28a50213
CarolinaVitale <vitale.carola@gmail.com>
2024-05-13 20:41:58 -0400

    menu

--- src/components/pages/Catering.js
+++ src/components/pages/Catering.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Header } from '../Header';
 import pic from '../../images/carpaccio.JPG'
+import MenuForm from '../Menu'
+
 
 function Catering() {
     return (
         <>
             <Header tittle='catering' image={pic} />
+            <div>
+                <MenuForm />
+            </div>
         </>
     );
 }
